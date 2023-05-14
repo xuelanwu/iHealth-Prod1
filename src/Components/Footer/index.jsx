abb7cbd37b3bd1facbf16fa542c6e7a299ca7127
XueLan Wu <xuelanwu823@gmail.com>
2023-05-14 17:54:45 -0400

    working on changing style

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -18,38 +18,36 @@ const Footer = () => {
   };
   return (
     <div className="footer-container">
-      <div className="footer-contact-container">
-        <div className="footer-contact-slogan-block">
-          <h2 className="footer-contact-slogan">
-            Stay informed about our latest news, exclusive resources, events and
-            opportunities.
-          </h2>
-        </div>
-        <div className="footer-contact-button-block">
-          <button className="stay-informed-bttn">
-            <MailOutlineIcon />
-            Stay Informed
-          </button>
-        </div>
-      </div>
       <div className="footer-inner">
         <div className="footer-top">
           <div className="footer-top-left">
             <div className="footer-address-block">
-              <h2>iHealth and Wellness Foundation, Inc.</h2>
+              <h3 className="footer-address-company">
+                iHealth and Wellness Foundation, Inc.
+              </h3>
               <p className="footer-address">
-                115 Broadway St. Fl 5, New York,<br></br> NY 10006, USA
+                115 Broadway St. Fl 5, New York, NY 10006, USA
               </p>
             </div>
+            <div className="footer-contact-block">
+              <h3 className="footer-contact-title">STAY IMFORMED:</h3>
+              <div className="footer-contact-subscribe-block">
+                <input placeholder="Your email here" />
+                <button className="stay-informed-bttn">subscribe</button>
+              </div>
+            </div>
             <div className="footer-social-media-block">
-              {SocialMedia.map((ele) => {
-                const Icon = ele.icon;
-                return (
-                  <a href={ele.url}>
-                    <Icon style={{ color: "white", fontSize: "2rem" }} />
-                  </a>
-                );
-              })}
+              <h3 className="footer-social-media-title">SOCIAL:</h3>
+              <div className="footer-social-media-icon-block">
+                {SocialMedia.map((ele) => {
+                  const Icon = ele.icon;
+                  return (
+                    <a href={ele.url}>
+                      <Icon style={{ color: "3D445C", fontSize: "2rem" }} />
+                    </a>
+                  );
+                })}
+              </div>
             </div>
           </div>
 
@@ -59,41 +57,41 @@ const Footer = () => {
             ))}
           </div>
         </div>
-        <div className="footer-bottom">
-          <div className="footer-terms-right-block">
-            <h5>
-              @2023 iHealth and Wellness Foundation, Inc. All rights reserved
-            </h5>
-          </div>
-          <div className="footer-language-block">
-            <Select
-              value={selectedLanguage}
-              onChange={handleChange}
-              sx={{
+      </div>
+      <div className="footer-bottom">
+        <div className="footer-terms-right-block">
+          <h5 className="footer-terms-right-content">
+            @2023 iHealth and Wellness Foundation, Inc. All rights reserved
+          </h5>
+        </div>
+        <div className="footer-language-block">
+          <Select
+            value={selectedLanguage}
+            onChange={handleChange}
+            sx={{
+              color: "white",
+              border: 0,
+              outline: 0,
+              ".MuiOutlinedInput-notchedOutline": {
+                borderColor: "rgba(228, 219, 233, 0)",
+              },
+              "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                borderColor: "rgba(228, 219, 233, 00)",
+              },
+              "&:hover .MuiOutlinedInput-notchedOutline": {
+                borderColor: "rgba(228, 219, 233, 0)",
+              },
+              "& .MuiSelect-icon": {
                 color: "white",
-                border: 0,
-                outline: 0,
-                ".MuiOutlinedInput-notchedOutline": {
-                  borderColor: "rgba(228, 219, 233, 0)",
-                },
-                "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-                  borderColor: "rgba(228, 219, 233, 00)",
-                },
-                "&:hover .MuiOutlinedInput-notchedOutline": {
-                  borderColor: "rgba(228, 219, 233, 0)",
-                },
-                "& .MuiSelect-icon": {
-                  color: "white",
-                },
-              }}
-            >
-              <MenuItem value="en">English</MenuItem>
-              <MenuItem value="cn">Chinese</MenuItem>
-              <MenuItem value="fr">Français</MenuItem>
-              <MenuItem value="es">Español</MenuItem>
-              <MenuItem value="de">Deutsch</MenuItem>
-            </Select>
-          </div>
+              },
+            }}
+          >
+            <MenuItem value="en">English</MenuItem>
+            <MenuItem value="cn">Chinese</MenuItem>
+            <MenuItem value="fr">Français</MenuItem>
+            <MenuItem value="es">Español</MenuItem>
+            <MenuItem value="de">Deutsch</MenuItem>
+          </Select>
         </div>
       </div>
     </div>
