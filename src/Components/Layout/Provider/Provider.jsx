f74e6c50673ac2d5ced1db3b6b0028596fd6959e
onehealthconnectapp <onehealthconnectapp@gmail.com>
2023-05-23 16:58:03 -0400

    Revert "Updated Provider.jsx"
    This reverts commit 451a5f32b7fc50c553d5e5f4f3e2d7a7eaa454a3.

--- src/Components/Layout/Provider/Provider.jsx
+++ src/Components/Layout/Provider/Provider.jsx
@@ -1,103 +1,91 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import "./Provider.css";
-import axios from "axios";
-//api url
-import { getProviders } from "src/config/urls";
 
 const Provider = ({ selected, setSelected }) => {
   const [isActive, setIsActive] = useState(false);
 
-  const options = ["Neurofibromatosis(NF)", "Select Disease"];
-  // const ProviderData = [
-  //   {
-  //     title: "Speciality",
-  //     text: "Select Speciality",
-  //     icon: <ExpandMoreIcon sx={{ fontSize: 18, width: 20 }} />,
-  //   },
-  //   {
-  //     title: "State",
-  //     text: "Enter State",
-  //     icon: <ExpandMoreIcon sx={{ fontSize: 16, width: 20 }} />,
-  //   },
-  // ];
-  //set providers
-  const [state, setState] = useState([]);
-  const [type, setType] = useState([]);
-  // fetch data from server(api)
-  useEffect(() => {
-    const getSpecialty = async () => {
-      const res = await axios.get(`${getProviders}`);
-      setState(res.data.fields.state);
-      setType(res.data.fields.specialty);
-    };
-    getSpecialty();
-  });
-  console.log(state);
-  return (
-    <div className="frame-container">
-      <div className="input-wrapper">
-        <div className="text-disease">
-          <small className="disease-header">Disease</small>
-          <div className="dropdown">
-            <span
-              className="dropdown-text"
-              onClick={(e) => setIsActive(!isActive)}
-            >
-              {selected} <ExpandMoreIcon sx={{ fontSize: 16, width: 20 }} />
-            </span>
-            {isActive && (
-              <div className="dropdown-content">
-                {options.map((option) => (
-                  <div
-                    onClick={(e) => {
-                      setSelected(option);
-                      setIsActive(false);
-                    }}
-                    className="dropdown-item"
-                  >
-                    {option}
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
-          <div className="patient-field">
-            <input type="checkbox" />
-            <label className="patient-status">Accepting new patients</label>
-          </div>
-        </div>
 
-        {/* {ProviderData.map((data) => ( */}
-        <>
-          {/* <div className="disease"> */}
-          <span className="data-text">Specialty</span>
-          <select className="type">
-            <option selected disabled>
-              Select Specialty
-            </option>
-            {type.map((items) => (
-              <option>{items}</option>
-            ))}
-          </select>
-          <span className="state-header">State</span>
-          <select className="state">
-            {state.map((items) => (
-              <option>{items}</option>
-            ))}
-          </select>
-          {/* </div> */}
-        </>
-        {/* ))} */}
+  const options = [
+  
+    "Neurofibromatosis(NF)", "Select Disease"];
+  const ProviderData = [
+    {
+      title: "Speciality",
+      text: "Select Speciality",
+      icon: <ExpandMoreIcon sx={{ fontSize: 18, width: 20 }} />,
+    },
+    {
+      title: "Age ",
+      text: "Select Age",
+      icon: <ExpandMoreIcon sx={{ fontSize: 16, width: 20 }} />,
+    },
+    {
+      title: "Insurance",
+      text: "Select Insurance",
+      icon: <ExpandMoreIcon sx={{ fontSize: 16, width: 20 }} />,
+    },
+   
+  ];
+  return (
+    
+    <div className="frame-container">
+      
+      <div className="input-wrapper">
+      <div className="text-disease">
+      <small>Disease</small>
+        <div className="dropdown">
+          <span className="dropdown-text" onClick={(e) => setIsActive(!isActive)} >
+            
+            {selected} <ExpandMoreIcon sx={{ fontSize: 16, width:20 }} />
+          </span>
+          {isActive && (
+            <div className="dropdown-content">
+              {options.map((option) => (
+                <div
+                  onClick={(e) => {
+                    setSelected(option);
+                    setIsActive(false);
+                  }} 
+              
+                  className="dropdown-item"
+                >
+                  {option }
+                </div>
+              ))}
+               
+            </div>
+          )}
+        </div>
+        </div>
+        
+
+        {ProviderData.map((data) => (
+          <>        
+            <div className="disease">
+            <span className="data-text">{data.title} </span>
+             <span className="data-title">
+             {data.text} {data.icon}
+             </span>
+             
+            </div>
+        
+          </>
+        ))}
+         <div className="zip-code">
+          <div className="small">
+          <small>Zip code</small>
+          </div>
+          
+          <input type="text" placeholder="Enter zipcode" />
+        </div>
+       
         <div className="search-donate">
           <div className="search-container">
-            <button className="search-btn">
-              {" "}
-              Search{" "}
-              <SearchOutlinedIcon sx={{ fontSize: 12, marginLeft: 0.7 }} />{" "}
-            </button>
+            <button className="search-btn" > Search <SearchOutlinedIcon sx={{fontSize:12, marginLeft:0.7}}/> </button> 
+            
           </div>
           <div className="frame-donate">
             <Link to="/donate" className="donate-now">
