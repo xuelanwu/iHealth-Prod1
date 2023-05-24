b998441a80c9316383e4922a045967d68fb7e4eb
Cem  Onal <mehmetcemonal@gmail.com>
2023-05-24 17:36:06 -0400

    logo style fixed / under-construction component created

---
+++ src/Components/Under-Constructure/UnderConstructure.jsx
@@ -0,0 +1,26 @@
+//Import Libraries/Packages
+import React from 'react'
+
+
+//Import Styles
+import "./UnderConstructure.css"
+
+
+//Import Local Files/Components
+
+
+
+
+function UnderConstructure() 
+{
+
+
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default UnderConstructure
