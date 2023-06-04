c4fd9cdb50d78d42d2eec2dce9f49911f3897ed2
XueLan Wu <xuelanwu823@gmail.com>
2023-06-04 19:37:46 -0400

    added padding after checkmark nfteam

--- src/Components/Home-Page/Layout/NfInformation/SubComponents/NfTeam-Info/NfTeam-Info.jsx
+++ src/Components/Home-Page/Layout/NfInformation/SubComponents/NfTeam-Info/NfTeam-Info.jsx
@@ -1,47 +1,47 @@
 //Import Libraries/Packages
-import React from 'react'
+import React from "react";
 
 //Import Styles
-import "./NfTeam-Info.css"
+import "./NfTeam-Info.css";
 
 //Import Local Files
-import {TeamInfo} from "../../../../../../Constants/NFTeams/NFTeam-Info.js"
+import { TeamInfo } from "../../../../../../Constants/NFTeams/NFTeam-Info.js";
 
-function NfTeamInfo() 
-{
-
-  
-
+function NfTeamInfo() {
   return (
     <>
-      <section id='nfteam-info'>
-
-        {
-            TeamInfo.map((arg,i) =>
-          {
-              return (
-                <div className='nfteam-info-card-wrapper' key={i+arg.image.alt}>
-                  <div className='nfteam-info-card-TOP'>
-                    <div><img src={arg.image.src} alt={arg.image.alt}/></div>
-                    <p>{arg.imageText}</p>
-                  </div>
-
-                  <div className='nfteam-info-card-BOTTOM'>
-                    <ol className='nfteam-info-list-wrapper'>
-                      <li>{arg.cardText1}</li>
-                      <li>{arg.cardText2}</li>
-                      <li>{arg.cardText3}</li>
-                    </ol>
-                  </div>
-                  <a onClick={()=>{document.querySelector('#UnderConst-wrapper').style.display = "flex"}}>Learn More &gt;</a>  
+      <section id="nfteam-info">
+        {TeamInfo.map((arg, i) => {
+          return (
+            <div className="nfteam-info-card-wrapper" key={i + arg.image.alt}>
+              <div className="nfteam-info-card-TOP">
+                <div>
+                  <img src={arg.image.src} alt={arg.image.alt} />
                 </div>
-              )
-          })
-        }
+                <p>{arg.imageText}</p>
+              </div>
 
+              <div className="nfteam-info-card-BOTTOM">
+                <ol className="nfteam-info-list-wrapper">
+                  <li>{arg.cardText1}</li>
+                  <li>{arg.cardText2}</li>
+                  <li>{arg.cardText3}</li>
+                </ol>
+              </div>
+              <a
+                onClick={() => {
+                  document.querySelector("#UnderConst-wrapper").style.display =
+                    "flex";
+                }}
+              >
+                Learn More &gt;
+              </a>
+            </div>
+          );
+        })}
       </section>
     </>
-  )
+  );
 }
 
-export default NfTeamInfo
+export default NfTeamInfo;
